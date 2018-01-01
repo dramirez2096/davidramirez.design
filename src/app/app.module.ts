9ada29bed30bbf6c3196a67194cd0f50ba14f7a3
David Ramirez <dramirez2096@gmail.com>
2017-12-31 23:48:05 -0800

    routes created, landing page route works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,11 @@ import { HamburgerComponent } from './components/hamburger/hamburger.component';
 import { LandingComponent } from './components/pages/landing/landing.component';
 import { AboutComponent } from './components/pages/about/about.component';
 
+
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +24,8 @@ import { AboutComponent } from './components/pages/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
