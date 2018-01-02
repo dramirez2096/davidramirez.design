568ca92aaa1a9a09cfa68ceb71a638a8d38ccb1a
David Ramirez <dramirez2096@gmail.com>
2018-01-01 18:32:57 -0800

    contact component created and routed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { LandingComponent } from './components/pages/landing/landing.component';
 import { AboutComponent } from './components/pages/about/about.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
@@ -19,7 +21,8 @@ const routes: Routes = [
     AppComponent,
     NavComponent,
     LandingComponent,
-    AboutComponent
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
