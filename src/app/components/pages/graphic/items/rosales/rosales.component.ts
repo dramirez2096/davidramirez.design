dc141e90db4443f96524ad7d01598d8c2b23e69a
David Ramirez <dramirez2096@gmail.com>
2018-01-06 22:26:08 -0800

    third graphic design item built

---
+++ src/app/components/pages/graphic/items/rosales/rosales.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-rosales',
+    templateUrl: './rosales.component.html',
+    styleUrls: ['./rosales.component.css'],
+})
+
+export class RosalesComponent {
+
+    constructor(){} 
+
+}
