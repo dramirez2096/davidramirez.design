b3de033fe548fa0dc8e0a7610c3a9fb0bc89ae7f
David Ramirez <dramirez2096@gmail.com>
2019-03-28 09:41:09 -0700

    add new projects

---
+++ src/app/components/pages/graphic/items/shift3/shift3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shift3',
+  templateUrl: './shift3.component.html',
+  styleUrls: ['./shift3.component.css']
+})
+export class Shift3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
